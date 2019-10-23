3bb1c7e29655194b922273bf3e3b1429ed9e5fb0
2017115214TenxMcu <1440271346@qq.com>
2019-10-23 12:39:14 +0800

    vscode 代码格式化 h 文件按字母排序导致 uint16_t 先使用后定义错误

--- prj0/code(78Bsop32)/includeAll.h
+++ prj0/code(78Bsop32)/includeAll.h
@@ -1,8 +1,10 @@
 #include <REGtenxTM52F5278B.h>
-#include <INTRINS.h>
 
 #include "typeAlias.h"
 
 #include "display.h"
 #include "init.h"
+#include "keys.h"
 #include "main.h"
+#include "typeAlias.h"
+#include <INTRINS.H>
