b26ca6110072161d35deb227438f334552cdfd72
2017115214TenxMcu <1440271346@qq.com>
2019-11-03 14:18:41 +0800

    初始化 T2 定时器，设置为 1ms 中断一次；

--- prj0/code(78Bsop32)/init.c
+++ prj0/code(78Bsop32)/init.c
@@ -4,7 +4,7 @@
 void SysInit() {
    F_clearWDT();
   //时钟初始化
-  CLKCON = 0x03; // Clock div 1
+  CLKCON = 0x03; // Clock div1  7.3728Mhz
   STPPCK = 0;
   STPFCK = 0;
   _nop_();
@@ -29,5 +29,14 @@ void SysInit() {
   P1_0 = 0;
   // IO 状态初始化
   F_ledOff();
+   // T2 初始化  7.3728Mhz/2 = 3686400Hz
+  //  3686400Hz/3686.4 = 1000Hz = 1ms timer2 interrupt
+  TH2 = (65536 - 3686) / 256;
+  RCP2H = TH2;
+  TL2 = (65536 - 3686) % 256;
+  RCP2L = TL2;
+  TR2 = 1;
+  ET2 = 1;
+  EA = 1;
 }
 void VarsInit() {}
