617512e411443246f4777f60aa041990ceef9fba
2017115214TenxMcu <1440271346@qq.com>
2019-11-29 16:45:45 +0800

    按键复用 P10，模式先改为上拉输入，使用后再恢复为输出

--- prj0/code(78Bsop32)/keys.c
+++ prj0/code(78Bsop32)/keys.c
@@ -5,12 +5,22 @@
 //=============================================================================
 void GetKeys() {
 	static uint8_t tmpKeyValue = D_keyNull;
+  //按键口 p10 设置为上拉输入
+  P1MODL  = 0xa8;
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
+  _nop_();
 
-  
+
   if (P_key1 == 0) {
     tmpKeyValue = D_keyValue1;
   }else{
     keyValue = tmpKeyValue ;
     tmpKeyValue = D_keyNull;  
 }
+//按键口P10,恢复为输出
+P1MODL = 0xaa;
 }
