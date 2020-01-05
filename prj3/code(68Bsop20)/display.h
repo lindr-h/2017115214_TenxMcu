b71a96fdb86510403d2206bbca10bb2be55d139c
2017115214TenxMcu <1440271346@qq.com>
2020-01-05 20:43:02 +0800

    数码管显示温度

--- prj3/code(68Bsop20)/display.h
+++ prj3/code(68Bsop20)/display.h
@@ -1,27 +1,29 @@
 #ifndef __display_h
 #define __display_h
-// Hal: exp: #define P_led_1 P1.1 -----------------
-#define P_led_1 P1_4
-#define P_led_2 P3_6
-#define P_led_com P1_0
+// Hal: exp: #define P_led P10 -----------------
+#define P_led P1_0
 // Const: exp: #define D_data 1 ----------------
-#define D_ledOn1 1  //高电位亮
-#define D_ledOff1 0 //低点位灭
-#define D_ledOn2 1  //高电位亮
-#define D_ledOff2 0 //低点位灭
+#define D_ledOn 0  //高电位亮
+#define D_ledOff 1 //低点位灭
 // Globle Var -----------------------------------------
 #ifdef __display_c
+bit bflag0;
+bit bflag;
 #else
-
+extern bit bflag0;
+extern bit bflag;
 #endif
 
 // Action Macro: exp: #define F_getData() ------
-#define F_ledOn1() P_led_1 = D_ledOn1
-#define F_ledOff1() P_led_1 = D_ledOff1
-#define F_ledNeg1() P_led_1 = ~P_led_1
-#define F_ledOn2() P_led_2 = D_ledOn2
-#define F_ledOff2() P_led_2 = D_ledOff2
-#define F_ledNeg2() P_led_2 = ~P_led_2
+#define F_ledOn() P_led = D_ledOn
+#define F_ledOff() P_led = D_ledOff
+#define F_ledNeg() P_led = ~P_led
 // Function ------------------------------------
-extern void LED_Disable(void);
+void DisplayNumber(uint8_t number);
+void DisplayTenNumber(uint8_t number);
+void DisplayBitNumber(uint8_t number);
+void DisplayOn(uint8_t number);
+void DisplayClear2();
+void DisplayLED12(void);
+void DisplayLED14(void);
 #endif
